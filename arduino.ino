50da5a804d570adfcecfce955d5c799b820a8480
brelynnn <41016205+brelynnn@users.noreply.github.com>
2018-07-11 11:38:24 -0400

    Add files via upload

---
+++ arduino.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+  int my_array[5]= {10,100,1000,10000};
+  int i;
+
+  Serial.begin(9600);
+
+  for (i=0; i<5; i++){
+    Serial.println(my_array[1]);
+  }
+}
+
+void loop(){
+}
+
